9b4681fd02db0f0ad82108deb429ff9fee865e26
mazensaadany <mazenelsaadany3@gmail.com>
2026-09-11 19:15:05 +0300

    1st answer

--- Program.cs
+++ Program.cs
@@ -1,16 +1,21 @@
 ﻿namespace RouteAss_1;
 
+//the class Book initialized
 class Book
 {
-    string Title;
-    int Pages;
+    public string Title;
+    public int Pages;
 }
 internal class Program
 {
     static void Main(string[] args)
     {
+        #region [1st answer]
+        Book book = new Book();
+        object obj = book;
 
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(obj);
+        #endregion
     }
 }
 
