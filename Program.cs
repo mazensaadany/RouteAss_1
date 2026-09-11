c42ecd59896c5b1474cbde93cbe2b3d29fa8e1bc
mazensaadany <mazenelsaadany3@gmail.com>
2026-09-11 19:23:18 +0300

    2nd answer

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@
 //the class Book initialized
 class Book
 {
-    public string Title;
-    public int Pages;
+    public string Title="refactoring";
+    public int Pages=464;
 }
 internal class Program
 {
@@ -15,6 +15,14 @@ internal class Program
         object obj = book;
 
         Console.WriteLine(obj);
+        #endregion
+        Console.WriteLine("_______________________________");
+
+        #region [2nd answer]
+        Console.WriteLine(book.ToString());
+        Console.WriteLine(book.Equals(book));
+        Console.WriteLine(book.GetHashCode());
+        Console.WriteLine(book.GetType());
         #endregion
     }
 }
